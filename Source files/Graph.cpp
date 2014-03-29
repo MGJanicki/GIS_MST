a7a135db686afee69f69558e18950ec1e1723770
Piotr Kalinowski <kali.piotrek@gmail.com>
2014-03-29 11:40:48 +0100

    przystosowanie projektu do biblioteki ogdf

--- Source files/Graph.cpp
+++ Source files/Graph.cpp
@@ -31,8 +31,8 @@ void Graph::addEdge(Edge *aEdge)
 	for(vector<Edge*>::iterator pIterator = edges.begin(); pIterator != edges.end(); ++pIterator)
 	{
 		//krawêdŸ nieskierowana - trzeba sprawdziæ czy graf ju¿ nie zawiera krawêdzi ³¹cz¹cej dan¹ parê wierzcho³ków 
-		if(((*pIterator)->getVertex1 == aEdge->getVertex1() && (*pIterator)->getVertex2 == aEdge->getVertex2())
-			|| ((*pIterator)->getVertex1 == aEdge->getVertex2() && (*pIterator)->getVertex2 == aEdge->getVertex1()))
+		if(((*pIterator)->getVertex1() == aEdge->getVertex1() && (*pIterator)->getVertex2() == aEdge->getVertex2())
+			|| ((*pIterator)->getVertex1() == aEdge->getVertex2() && (*pIterator)->getVertex2() == aEdge->getVertex1()))
 			return;
 	}
 	edges.push_back(aEdge);
--- Source files/main.cpp
+++ Source files/main.cpp
@@ -1,9 +1,25 @@
 #include "../Headers/Graph.h"
 #include <iostream>
 #include <stdlib.h>
+#include <ogdf/basic/tuples.h>
+#include <ogdf/basic/Graph_d.h>
+#include <ogdf/planarity/PlanarizationLayout.h>
+#include <ogdf/planarity/VariableEmbeddingInserter.h>
+#include <ogdf/planarity/FastPlanarSubgraph.h>
+#include <ogdf/orthogonal/OrthoLayout.h>
+#include <ogdf/planarity/EmbedderMinDepthMaxFaceLayers.h>
 
+using namespace ogdf;
 int main(int argc, char* argv[])
 {
+	ogdf::Graph G;
+	node n1 = G.newNode();
+	node n2 = G.newNode();
+	G.newEdge(n1,n2);
+	G.newEdge(n1,n2);
+	cout << G.numberOfEdges() << endl;
 	system("PAUSE");
-	return 0;
+ 
+    return 0;
+
 }
